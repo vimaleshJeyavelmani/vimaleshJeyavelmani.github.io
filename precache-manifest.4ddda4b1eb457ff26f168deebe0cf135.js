e970132552f05cb3621c575ac9e14da39023797c
vimalesh <vimaleshbe@gmail.com>
2019-09-09 20:50:04 +0530

    updated with new profile

---
+++ precache-manifest.4ddda4b1eb457ff26f168deebe0cf135.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b45c7815995e5a073de19746ec831b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c90080e8fb7c056ac237",
+    "url": "/static/css/main.c1105e5d.chunk.css"
+  },
+  {
+    "revision": "cb90eee17d5c7d550a5a",
+    "url": "/static/js/2.477e10bc.chunk.js"
+  },
+  {
+    "revision": "c90080e8fb7c056ac237",
+    "url": "/static/js/main.7024746a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
